b87ea59e4951ae7a9595f430038c1f5bc501ecc9
addy <addyjaime@gmail.com>
2025-02-25 06:15:54 -0500

    server running succefully

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+import sequelize from "@config/database";
+import env from "@config/env";
+
+export { sequelize, env };
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import sequelize from "@config/database";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/ping", (_req, res) => {
+  console.log("server hit");
+  res.json({ message: "hello server" });
+});
+
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () =>
+      console.log(`🚀 Server running in  http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log("❌ Error connecting to the database", error);
+  }
+};
+
+startServer();
