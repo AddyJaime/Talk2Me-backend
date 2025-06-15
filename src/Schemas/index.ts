5905eff77b88cdc56c9cf288c9b3ce591270739e
addy <addyjaime@gmail.com>
2025-06-15 14:24:52 -0400

    this version is working

--- src/Schemas/index.ts
+++ src/Schemas/index.ts
@@ -1,6 +1,6 @@
 import DatabaseCredentialsSchema from "./DatabaseCredentialsSchema";
 import loginSchema from "./loginSchema";
 import registerSchema from "./registerSchema";
-import friendshipSchema from "./friendshipSchema";
 
-export { DatabaseCredentialsSchema, loginSchema, registerSchema, friendshipSchema };
+
+export { DatabaseCredentialsSchema, loginSchema, registerSchema };
