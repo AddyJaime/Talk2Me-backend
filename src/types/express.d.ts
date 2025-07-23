46628800294f720880e8f6181313361f700a5b8f
addy <addyjaime@gmail.com>
2025-07-23 19:34:53 -0400

    cleaning a little for adding more routes

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,13 +1,9 @@
 import { User } from "@src/models";
 import { JwtPayload } from "jsonwebtoken";
-
-// Para decirle a TS qué tiene tu token
-
 export interface Userpayload extends JwtPayload {
   id: number,
 
 }
-// para decrile a typscripqe que req.user si exsite 
 declare global {
   namespace Express {
     interface Request {
@@ -15,5 +11,5 @@ declare global {
     }
   }
 }
-// esto le indca  atypescript quee este archivo es un modulo y no un archivo globa 
+
 export { }
